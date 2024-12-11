fdb44ede61adc0d8e9e1ef9ca189aee986f8f5e0
jaydenhuang555 <jaydenkanahuang@outlook.com>
2024-12-11 14:21:17 -0800

    built on vscode

--- src/main/java/org/jaq/daebak/client/app/App.java
+++ src/main/java/org/jaq/daebak/client/app/App.java
@@ -675,6 +675,7 @@ the library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.  But first, please read
 <https://www.gnu.org/licenses/why-not-lgpl.html>.
  */
+
 import net.kyori.adventure.text.Component;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
