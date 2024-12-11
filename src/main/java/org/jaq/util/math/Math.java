413826e87d46259758636368756e6d9fdd4b28da
jaydenhuang555 <jaydenkanahuang@outlook.com>
2024-12-11 09:54:54 -0800

    created branch

---
+++ src/main/java/org/jaq/util/math/Math.java
@@ -0,0 +1,16 @@
+package org.jaq.util.math;
+
+import org.jaq.util.OrderedList;
+import org.jaq.util.math.token.Token;
+
+public class Math {
+
+
+    
+    
+    public static Double eval(String eq){
+        OrderedList<Token> tokens = new OrderedList<>();
+        return 0;
+    }
+
+}
