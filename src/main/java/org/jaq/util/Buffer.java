91a44c8d34ede07aac7014c2b6ed8e3635571752
jaydenhuang555 <jaydenkanahuang@outlook.com>
2024-12-12 20:37:44 -0800

    added nothing :)

--- src/main/java/org/jaq/util/Buffer.java
+++ src/main/java/org/jaq/util/Buffer.java
@@ -1,8 +1,30 @@
 package org.jaq.util;
+/*
+* experimental class to try to get a form of operator overloading
+* */
+/*
+import com.jops.annotation.OperatorOverloading;
 
+@OperatorOverloading
 public class Buffer {
 
     private char buffer[];
     private int size, cap;
 
+    public Buffer(){
+        this.size = 0;
+        this.cap = 1 << 3;
+        this.buffer = new char[this.cap];
+    }
+
+    public Buffer operator+(Buffer buff){
+        return null;
+    }
+
+    public void test(){
+        new Buffer() + new Buffer();
+    }
+
+
 }
+*/
